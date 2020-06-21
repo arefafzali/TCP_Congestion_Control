9aaa660148cf246c2a831e42bc0440b659956e16
Aref Afzali <afzaliaref.aa@gmail.com>
2020-06-21 16:53:26 +0430

    upload

---
+++ process_trace_file.py
@@ -0,0 +1,118 @@
+import matplotlib.pyplot as plt
+import subprocess
+import multiprocessing as mp
+import sys
+
+def corresponding_send_idx(time, source_node):
+	f = open("trace.tr", "r")
+	lines = f.readlines()
+	for l in lines:
+		l = l.split()
+		if(l[-2] != "maxseq_"):
+			continue
+		if l[0] == time:
+			if l[1] == source_node:
+				return l[-1]
+	return False
+
+def process_trace_var(trace_var_):
+	x = [[], []]
+	y = [[], []]
+	f = open("trace.tr", "r")
+	lines = f.readlines()
+	num_send_pkt = [0, 0]
+	for line_num, l in enumerate(lines):
+		l = l.split()
+		if l[0] == "-":
+			if l[2] == "2":
+				num_send_pkt[0] += 1
+			elif l[2] == "3":
+				num_send_pkt[1] += 1
+
+		trace_var = l[-2]
+		if trace_var == trace_var_:
+			num_tcp = int(l[1]) - 2
+			x_trace_val = float(l[0])
+			y_trace_val = float(l[-1])
+			if y_trace_val >= 0 :
+					if trace_var == "ack_":
+						if num_send_pkt[num_tcp] != 0:
+							x[num_tcp].append(x_trace_val)
+							y[num_tcp].append((y_trace_val) / num_send_pkt[num_tcp])
+					else:
+						x[num_tcp].append(x_trace_val)
+						y[num_tcp].append(y_trace_val)
+
+		#if line_num >= 20000:
+			#break
+	f.close()
+	return [x, y]
+
+def plot_output(x, y, l, var):
+	# plt.figure()
+	plt.plot(x[0], y[0], label="tcp_1 "+var)
+	plt.plot(x[1], y[1], label="tcp_2 "+var)
+	plt.legend(loc="best")
+	plt.title(l)
+	if var =="goodput":
+		plt.ylim([-0.2, 1])
+
+def count_drop_pkt():
+	num_drop = [0, 0]
+	f = open("trace.tr", "r")
+	lines = f.readlines()
+	for l in lines:
+		l = l.split()
+		if l[0] == "d":
+			if l[7] == "1":
+				num_drop[0] += 1
+			elif l[7] == "2":
+				num_drop[1] += 1
+			else :
+				print("l[7]={}, Number of Flows is greater than 2".format(l[7]))
+				sys.exit(1)
+	return num_drop
+
+if __name__ == "__main__":
+	pool = mp.Pool(processes=10)
+
+	#for i in range(2):
+	print("TCP New-Reno:")
+	subprocess.run(["ns", sys.argv[1]])
+	data1 = pool.map(process_trace_var, ["cwnd_", "ack_", "rtt_"])
+	num_drop = count_drop_pkt()
+	print("Num Drops For TCP_1 = {}".format(num_drop[0]))
+	print("Num Drops For TCP_2 = {}\n".format(num_drop[1]))
+
+	print("TCP Tahoe:")
+	subprocess.run(["ns", sys.argv[2]])
+	data2 = pool.map(process_trace_var, ["cwnd_", "ack_", "rtt_"])
+	num_drop = count_drop_pkt()
+	print("Num Drops For TCP_1 = {}".format(num_drop[0]))
+	print("Num Drops For TCP_2 = {}\n".format(num_drop[1]))
+
+	print("TCP Vegas:")
+	subprocess.run(["ns", sys.argv[3]])
+	data3 = pool.map(process_trace_var, ["cwnd_", "ack_", "rtt_"])
+	num_drop = count_drop_pkt()
+	print("Num Drops For TCP_1 = {}".format(num_drop[0]))
+	print("Num Drops For TCP_2 = {}\n".format(num_drop[1]))
+	
+	pool.close()
+	plt.figure()
+	plot_output(data1[0][0], data1[0][1], "CWND", "Newreno cwnd")
+	plot_output(data2[0][0], data2[0][1], "CWND", "Tahoe cwnd")
+	plot_output(data3[0][0], data3[0][1], "CWND", "Vegas cwnd")
+	print("CWND plot is complete")
+	plt.figure()
+	plot_output(data1[1][0], data1[1][1], "Good Put Ratio", "Newreno goodput")
+	plot_output(data2[1][0], data2[1][1], "Good Put Ratio", "Tahoe goodput")
+	plot_output(data3[1][0], data3[1][1], "Good Put Ratio", "Vegas goodput")
+	print("Good put ratio plot is complete")
+	plt.figure()
+	plot_output(data1[2][0], data1[2][1], "RTT", "Newreno rtt")
+	plot_output(data2[2][0], data2[2][1], "RTT", "Tahoe rtt")
+	plot_output(data3[2][0], data3[2][1], "RTT", "Vegas rtt")
+	print("RTT plot is complete")
+	
+	plt.show()
---
+++ tcp_vegas.tcl
@@ -0,0 +1,155 @@
+#Create sim obj
+set ns [new Simulator]
+set random_delay [expr {5+(rand()*20)}]
+puts $random_delay
+
+#Color of data flow between server 1 and 5
+$ns color 1 Blue
+#Color of data flow between server 2 and 6
+$ns color 2 Red
+
+#Create Nam file
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+#Create Trace file
+set tracefile [open trace.tr w]
+$ns trace-all $tracefile
+
+#Define a 'finish' procedure
+proc finish {} {
+        global ns nf tracefile tcp_1 tcp_2 
+        $ns flush-trace
+        #Close the NAM trace file
+        close $nf
+	set tcp_tick_1 [$tcp_1 set tcpTick_]
+	set window_1 [$tcp_1 set window_]
+	set last_ack_1 [$tcp_1 set ack_]
+	set last_seq_1 [$tcp_1 set maxseq_]
+
+	set tcp_tick_2 [$tcp_2 set tcpTick_]
+	set window_2 [$tcp_2 set window_]
+	set last_ack_2 [$tcp_2 set ack_]
+	set last_seq_2 [$tcp_2 set maxseq_]
+        #Execute NAM on the trace file
+        #exec nam out.nam &
+	puts "tcp_1 window size: $window_1, tcp_2 window size: $window_2"
+	puts "tcp_tick_1 :$tcp_tick_1 last_ack_1 :$last_ack_1 - last_seq_1 :$last_seq_1" 
+	puts "tcp_tick_2 :$tcp_tick_2 last_ack_2 :$last_ack_2 - last_seq_2 :$last_seq_2"
+	close $tracefile
+        exit 0
+}
+
+#define routers
+set r1 [$ns node]
+set r2 [$ns node]
+set node_1 [$ns node]
+set node_2 [$ns node]
+set node_3 [$ns node]
+set node_4 [$ns node]
+
+#Set First TCP Connection
+set tcp_1 [new Agent/TCP/Vegas]
+$tcp_1 set class_ 1
+$tcp_1 set fid_ 1
+$tcp_1 set packetSize_ 960
+
+#Set Sink for TCP_1
+set sink_1 [new Agent/TCPSink]
+
+#Set Second TCP Connection
+set tcp_2 [new Agent/TCP/Vegas]
+$tcp_2 set class_ 2
+$tcp_2 set fid_ 2
+$tcp_2 set packetSize_ 960
+
+#Set Sink for TCP_2
+set sink_2 [new Agent/TCPSink]
+
+
+#Set FTP
+set ftp_1 [new Application/FTP]
+#$ftp_1 set type_ FTP
+set ftp_2 [new Application/FTP]
+#$ftp_2 set type_ FTP
+
+
+#Set Connection between Nodes
+$ns duplex-link $r1 $r2 100kb 1ms DropTail
+$ns duplex-link $node_1 $r1 100Mb 5ms DropTail
+$ns duplex-link $node_2 $r1 100Mb ${random_delay}ms DropTail
+$ns duplex-link $r2 $node_3 100Mb 5ms DropTail
+$ns duplex-link $r2 $node_4 100Mb ${random_delay}ms DropTail
+
+#Set Links between TCP Connection and nodes
+$ns attach-agent $node_1 $tcp_1
+$ns attach-agent $node_2 $tcp_2
+$ns attach-agent $node_3 $sink_1
+$ns attach-agent $node_4 $sink_2
+
+#Set Link between TCP connection and FTP
+$ftp_1 attach-agent $tcp_1
+$ftp_2 attach-agent $tcp_2
+
+#Connect TCP pair
+$ns connect $tcp_1 $sink_1
+$ns connect $tcp_2 $sink_2
+
+#Trace TCP Var
+# Let's trace some variables
+$tcp_1 attach $tracefile
+$tcp_2 attach $tracefile
+
+$tcp_1 tracevar cwnd_
+$tcp_1 tracevar ssthresh_
+$tcp_1 tracevar ack_
+$tcp_1 tracevar maxseq_
+$tcp_1 tracevar rtt_
+
+$tcp_2 tracevar cwnd_
+$tcp_2 tracevar ssthresh_
+$tcp_2 tracevar ack_
+$tcp_2 tracevar maxseq_
+$tcp_2 tracevar rtt_
+
+#$sink_1 tracevar cwnd_
+#$sink_1 tracevar ssthresh_
+#$sink_1 tracevar ack_
+#$sink_1 tracevar maxseq_
+
+
+#$sink_2 tracevar cwnd_
+#$sink_2 tracevar ssthresh_
+#$sink_2 tracevar ack_
+#$sink_2 tracevar maxseq_
+
+
+#Set Queue Size of Links
+$ns queue-limit $r1 $r2 10
+#Give node position (for NAM)
+$ns duplex-link-op $node_1 $r1 orient right-down
+$ns duplex-link-op $node_2 $r1 orient right-up
+$ns duplex-link-op $r1 $r2 orient right
+$ns duplex-link-op $r2 $node_3 orient right-up
+$ns duplex-link-op $r2 $node_4 orient right-down
+
+#Monitor the queue for link (node_2-node_3). (for NAM)
+$ns duplex-link-op $r2 $r1 queuePos 0.5
+
+
+#Schedule FTP
+$ns at 1 "$ftp_1 start"
+$ns at 1 "$ftp_2 start"
+$ns at 990 "$ftp_1 stop"
+$ns at 990 "$ftp_2 stop"
+
+#Detach tcp and sink agents (not really necessary)
+$ns at 999 "$ns detach-agent $node_1 $tcp_1 ; $ns detach-agent $node_2 $tcp_2 ; $ns detach-agent $node_3 $sink_1 ; $ns detach-agent $node_4 $sink_2"
+
+#Call the finish procedure after 5 seconds of simulation time
+$ns at 1000 "finish"
+
+#Run the simulation
+$ns run
+
+
